efddd2bc884a31ded81b1d0c84d649fe89fa9494
Abhishek Kumar <akabhishek2509@gmail.com>
2026-01-31 23:01:44 +0530

    Add files via upload

---
+++ CountEvenNum.java
@@ -0,0 +1,13 @@
+// Count even number present in user entered number 
+import java.util.Scanner;
+public class CountEvenNum {
+    public static void main(String[] a){
+        Scanner abc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = abc.nextInt();
+        while(n>0){
+        int d = n % 10;
+        
+        }
+    }
+}
